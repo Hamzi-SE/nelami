b1f4f08ce75bb2e3deef1165a40dc8cbe66aefff
Hamza <hamzii.se@gmail.com>
2024-07-18 23:03:45 +0500

    updated meta data

--- frontend/src/utils/MetaData.js
+++ frontend/src/utils/MetaData.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Helmet from "react-helmet";
+import { Helmet } from "react-helmet";
 
 const MetaData = ({ title, description }) => {
     return (
         <Helmet>
             <title>{title}</title>
-            <meta name="description" content={description ? description : "Nelami is a platform for buying and selling of vehicles, properties, and other items."} />
+            <meta name="description" content={description ? description : "Nelami is a platform where you can buy, sell, auction and bid for vehicles, properties and other items."} />
             <meta name="keywords" content="Nelami, Buy, Sell, Auction, Bidding, Vehicles, Properties, Items" />
+            <link rel="canonical" href="https://nelami.ihamza.dev/" />
 
         </Helmet>
     );
