c8a3f26a703a56680eee3f8bc1e64e0ebf410a01
Hamza <hamzii.se@gmail.com>
2024-08-07 22:26:11 +0500

    fixed a bug by adding 'return' in pre save function

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -93,7 +93,7 @@ const userSchema = new mongoose.Schema(
 // Hashing the Password
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) {
-    next()
+    return next()
   }
 
   this.password = await bcrypt.hash(this.password, 10)
